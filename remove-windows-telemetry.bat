00c1fcfc897b314116076da719eb68389dbd0925
Artur Rodrigues Rocha Neto <artur.rodrigues26@gmail.com>
2019-02-11 12:24:38 -0200

    Update remove-windows-telemetry.bat
    ok, agora sim ashuahusuha

--- remove-windows-telemetry.bat
+++ remove-windows-telemetry.bat
@@ -4,24 +4,77 @@ echo keizerzilla's Windows 7/8/8.1 telemetry/spyware update removal tool
 echo credits: reddit, duckduckgo and Nalal@github
 
 :: removing some bad updates
+echo removing kb:2952664...
 wusa /uninstall /kb:2952664 /norestart /quiet
+echo done!
+
+echo removing kb:2976978...
 wusa /uninstall /kb:2976978 /norestart /quiet
+echo done!
+
+echo removing kb:2977759...
 wusa /uninstall /kb:2977759 /norestart /quiet
+echo done!
+
+echo removing kb:2990214...
 wusa /uninstall /kb:2990214 /norestart /quiet
+echo done!
+
+echo removing kb:3021917...
 wusa /uninstall /kb:3021917 /norestart /quiet
+echo done!
+
+echo removing kb:3022345...
 wusa /uninstall /kb:3022345 /norestart /quiet
+echo done!
+
+echo removing kb:3035583...
 wusa /uninstall /kb:3035583 /norestart /quiet
+echo done!
+
+echo removing kb:3044374...
 wusa /uninstall /kb:3044374 /norestart /quiet
+echo done!
+
+echo removing kb:3068708...
 wusa /uninstall /kb:3068708 /norestart /quiet
+echo done!
+
+echo removing kb:3075249...
 wusa /uninstall /kb:3075249 /norestart /quiet
+echo done!
+
+echo removing kb:3080149...
 wusa /uninstall /kb:3080149 /norestart /quiet
+echo done!
+
+echo removing kb:3015249...
 wusa /uninstall /kb:3015249 /norestart /quiet
+echo done!
+
+echo removing kb:3050267...
 wusa /uninstall /kb:3050267 /norestart /quiet
-wusa /uninstall /KB:3139929 /norestart /quiet
-wusa /uninstall /KB:3146449 /norestart /quiet
-wusa /uninstall /KB:3083710 /norestart /quiet
-wusa /uninstall /KB:3083324 /norestart /quiet
-wusa /uninstall /KB:971033 /norestart /quiet
+echo done!
+
+echo removing kb:3139929...
+wusa /uninstall /kb:3139929 /norestart /quiet
+echo done!
+
+echo removing kb:3146449...
+wusa /uninstall /kb:3146449 /norestart /quiet
+echo done!
+
+echo removing kb:3083710...
+wusa /uninstall /kb:3083710 /norestart /quiet
+echo done!
+
+echo removing kb:3083324...
+wusa /uninstall /kb:3083324 /norestart /quiet
+echo done!
+
+echo removing kb:971033...
+wusa /uninstall /kb:971033 /norestart /quiet
+echo done!
 
 :: disabling Diagtrack
 sc stop Diagtrack
