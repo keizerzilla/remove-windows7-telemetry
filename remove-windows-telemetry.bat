f9fb042577e19ec85565d4e7c103f0a80d0c2038
Artur Rodrigues Rocha Neto <artur.rodrigues26@gmail.com>
2019-02-11 10:59:11 -0200

    Update and rename telemetry-remove.bat to remove-windows-telemetry.bat

--- remove-windows-telemetry.bat
+++ remove-windows-telemetry.bat
@@ -1,3 +1,8 @@
+@echo off
+color 1d
+echo keizerzilla's Windows 7/8/8.1 telemetry/spyware update removal tool
+echo credits: reddit, duckduckgo and Nalal@github
+
 :: removing some bad updates
 wusa /uninstall /kb:2952664 /norestart /quiet
 wusa /uninstall /kb:2976978 /norestart /quiet
@@ -10,10 +15,18 @@ wusa /uninstall /kb:3044374 /norestart /quiet
 wusa /uninstall /kb:3068708 /norestart /quiet
 wusa /uninstall /kb:3075249 /norestart /quiet
 wusa /uninstall /kb:3080149 /norestart /quiet
+wusa /uninstall /kb:3015249 /norestart /quiet
+wusa /uninstall /kb:3050267 /norestart /quiet
+wusa /uninstall /KB:3139929 /norestart /quiet
+wusa /uninstall /KB:3146449 /norestart /quiet
+wusa /uninstall /KB:3083710 /norestart /quiet
+wusa /uninstall /KB:3083324 /norestart /quiet
+wusa /uninstall /KB:971033 /norestart /quiet
 
 :: disabling Diagtrack
 sc stop Diagtrack
 sc delete Diagtrack
+echo (If you see "service does not exist" error = it was never on your system)
 
 :: fin
 pause
